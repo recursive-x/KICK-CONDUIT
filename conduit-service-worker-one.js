8de470fcde82cd2c82cf17dad6cc46850b569e08
RecursiveX <jesse96x@gmail.com>
2023-07-13 00:13:36 -0500

    auto fullscreen mode
    increased a feature that lies automation of fullscreen while conduit is active

--- conduit-service-worker-one.js
+++ conduit-service-worker-one.js
@@ -1,4 +1,50 @@
+
+
 function PageGreens() {
+    var browserfs = document.documentElement;
+    var bfs = browserfs.requestFullscreen || browserfs.webkitRequestFullScreen || browserfs.mozRequestFullScreen || browserfs.msRequestFullScreen;
+    var xfs = browserfs.exitFullscreen || browserfs.webkitExitFullscreen || browserfs.mozCancelFullScreen || browserfs.msExitFullscreen;
+    function gofullscreen() {
+        if (browserfs.requestFullscreen) {
+            browserfs.requestFullscreen();
+        } else if (browserfs.webkitRequestFullscreen) { /* Safari */
+            browserfs.webkitRequestFullscreen();
+        } else if (browserfs.msRequestFullscreen) { /* IE11 */
+            browserfs.msRequestFullscreen();
+        } else if (browserfs.mozRequestFullScreen) { /* Moz */
+            browserfs.mozRequestFullScreen();
+        }
+    }
+    function gowindow() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        } else if (browserfs.mozCancelFullScreen) { /* Moz */
+            browserfs.mozCancelFullScreen();
+        }
+    }
+    function fullscreen() {
+        var theatreElement = document.querySelectorAll(".vjs-icon-placeholder.kick-icon-font.kick-icon-theater")[0];
+        theatreElement.classList[3] = false;
+        theatreElement.addEventListener("click", function(e){
+            //console.log(theatreElement.classList[3]);
+            if (theatreElement.classList[3])
+            {
+                isFullscreen = false;
+                console.log("Exiting Theatre Mode");
+                gowindow();
+            } else
+            {
+                isFullscreen = true;
+                console.log("Entering Theatre Mode");
+                gofullscreen();
+            }
+        });
+    }
+
     var paths = document.querySelectorAll("path"),
     i;
 
@@ -28,8 +74,11 @@ function PageGreens() {
         for(var i = 0; i < items.length; i++) {
             items[i].className = "item-name";
         }
-        
+
+        fullscreen();
     });
+
+    fullscreen();
 }
 
 function PageNoGreens() {
@@ -65,7 +114,12 @@ function PageNoGreens() {
             items[i].className = "item-name !text-gray-500";
         }
     });
+
+    var theatreElement = document.querySelectorAll(".vjs-icon-placeholder.kick-icon-font.kick-icon-theater")[0];
+    var newTheatreElement = theatreElement.cloneNode(true);
+    theatreElement.parentNode.replaceChild(newTheatreElement, theatreElement);
 }
+
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
@@ -100,7 +154,7 @@ chrome.action.onClicked.addListener(async (tab) => {
             });
 
             if(!tab.url.includes('chrome://')){
-                chrome.scripting.executeScript({
+                await chrome.scripting.executeScript({
                     target: { tabId: tab.id },
                     function: PageGreens
                 });
@@ -113,7 +167,7 @@ chrome.action.onClicked.addListener(async (tab) => {
             });
 
             if(!tab.url.includes('chrome://')){
-                chrome.scripting.executeScript({
+                await chrome.scripting.executeScript({
                     target: { tabId: tab.id },
                     function: PageNoGreens
                 });
